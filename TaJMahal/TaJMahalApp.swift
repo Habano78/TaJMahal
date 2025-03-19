4060ecb94dd5b6511a13880096c56bcefce6cf3a
William-Gabriel <willx_gabriel@yahoo.com>
2025-03-19 22:54:03 +0100

    Initial Commit

---
+++ TaJMahal/TaJMahalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaJMahalApp.swift
+//  TaJMahal
+//
+//  Created by Perez William on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaJMahalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
