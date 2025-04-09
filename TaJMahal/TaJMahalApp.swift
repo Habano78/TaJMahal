d2bdc998b4b9d555fd34ff33bbad093f98640483
William-Gabriel <willx_gabriel@yahoo.com>
2025-04-09 09:10:42 +0200

    Clean

--- TaJMahal/TaJMahalApp.swift
+++ TaJMahal/TaJMahalApp.swift
@@ -9,9 +9,6 @@ import SwiftUI
 
 @main
 struct TaJMahalApp: App {
-        // On crée une instance unique du ViewModel qui sera qui passera les données aux autres vues. 
-        let viewModel = ViewModel()
-        
         var body: some Scene {
                 WindowGroup {
                         //MARK: la NavigationStack doit être définie au niveau de l'app. Cohèrence et optimisation : elle devienne global pour toute l'application
